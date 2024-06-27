0280b1d82c832a0a9f6461ac1cd9a2e93cce1aea
rasikww <rasikauom@gmail.com>
2024-06-27 22:45:30 +0530

    add employee to the database using web page completed using angular 18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ManageEmployeeComponent } from './page/manage-employee/manage-employee.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ManageEmployeeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'emp-app';
